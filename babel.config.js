c7bc9b56bd5dc9a9a3b3330e396afa9ed6369cfc
Victor Augusto Ferreira <victoraugusferreira@gmail.com>
2021-08-19 22:40:22 -0300

    Add icon and splashscreen

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ts', '.tsx', '.jsx', '.js', '.json'],
+        alias: {
+          '@components': './src/components',
+          '@contexts': './src/contexts',
+          '@screens': './src/screens',
+          '@assets': './src/assets',
+          '@types': './src/types',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
